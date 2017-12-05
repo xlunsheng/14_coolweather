eabc09393c8f1850fd8fa61789a6be395d8ee865
xlunsheng <xlslove2008@163.com>
2017-12-05 17:26:39 +0800

    完成遍历省市县三级列表的功能。

--- app/src/main/java/com/ubicell/a14_coolweather/util/Utility.java
+++ app/src/main/java/com/ubicell/a14_coolweather/util/Utility.java
@@ -19,7 +19,7 @@ public class Utility {
      * 解析和处理服务器返回的省级数据
      */
     public static boolean handleProvinceResponse(String response){
-        if(TextUtils.isEmpty(response)){
+        if(!TextUtils.isEmpty(response)){
             try {
                 JSONArray allProvinces = new JSONArray(response);
                 for (int i=0;i<allProvinces.length();i++){
