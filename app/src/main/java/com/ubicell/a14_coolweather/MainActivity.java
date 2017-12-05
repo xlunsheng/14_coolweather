03620f9636f970dc54ac47b8936d68949f8a183a
xlunsheng <xlslove2008@163.com>
2017-12-05 09:54:57 +0800

    First3 commit.

--- app/src/main/java/com/ubicell/a14_coolweather/MainActivity.java
+++ app/src/main/java/com/ubicell/a14_coolweather/MainActivity.java
@@ -3,6 +3,9 @@ package com.ubicell.a14_coolweather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//Git:
+//https://github.com/xlunsheng/14_coolweather
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
